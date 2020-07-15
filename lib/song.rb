ef3aadbc965fd775d973225d58f3bcadc90b0c25
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-15 14:12:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    return @@artists.uniq
+  end
+
+  def self.genres
+    return @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |word|
+      # binding.pry
+      if genre_hash[word]
+        genre_hash[word] += 1
+      else
+        genre_hash[word]
+      end
+    end
+    return genre_hash
+  end
+
+  def self.artist_count
+    artist_hash = {}
+    @@artists.each do |word|
+      # binding.pry
+      endartist_hash[word] = 1
+    end
+    return artist_hash
+  end
+
+end
