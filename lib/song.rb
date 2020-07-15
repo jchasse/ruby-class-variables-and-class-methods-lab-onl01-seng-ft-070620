b280b2a046e3b1eb1d4fe8f366dbeeb45beafaa7
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-15 14:24:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,11 +45,11 @@ class Song
     artist_hash = {}
     @@artists.each do |element|
       binding.pry
-      if artist_hash[element] == nil
-        artist_hash[element] = 1
-      else
+      # if artist_hash[element] == nil
+      #   artist_hash[element] = 1
+      # else
         artist_hash[element] = artist_hash[element].count + 1
-      end
+      # end
     end
     return artist_hash
   end
